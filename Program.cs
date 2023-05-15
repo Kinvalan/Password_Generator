e5370a65a416f477712dd6732f68b6e5f9ca5147
Kinvalan <sebastianmoller01@icloud.com>
2023-05-15 12:49:17 +0200

    Update Program.cs
    Done

--- Program.cs
+++ Program.cs
@@ -5,86 +5,132 @@ Det vil si at passordet skal bestå av 14 tegn, med en liten bokstav, en storbok
 */
 
 
+using System.Runtime.InteropServices;
+
 namespace Password_Generator
 {
     internal class Program
     {
+        static bool _running = true;
+
         static void Main(string[] args)
         {
-
-            var isRunning = true;
-
-            while (isRunning)
+            while (_running)
             {
-                if(args.Length == 0)
-                {
-                    Console.WriteLine("det er ingen parametere");
-                    infoForUser();
-                    isRunning = false;
-                }
-                
-                else if (args.Length == 2 && isInputANumber(args[0]) && isInputADesiredLetter(args[1])) 
-                  /*
-                  Hvis det er akkurat to kommandolinjeparametre og
-                  det første parameteret er et siffer og
-                  det andre parameteret består av de bokstavene jeg valgte
-                  */
-                {
-                    Console.WriteLine("det er to parametere, det første er sifre og det andre er godkjente bokstaver");
-                    isInputANumber(args[0]);
-                    isInputADesiredLetter(args[1]);
+                Console.Clear();
+                PasswordGenerator(args);
+                Console.WriteLine("\n\n\nPress Escape to exit, or any other key to generate a new password.");
+                var key = Console.ReadKey();
 
-                }
-                
-                else if(args.Length != 2)// Hvis det IKKE er akkurat to kommandolinsjeparametre
-                {
-                    Console.WriteLine("det er ikke akkurat to parametere");
-                    infoForUser();
-                    isRunning = false;
-                }
+                if (key.Key == ConsoleKey.Escape) Environment.Exit(0);
             }
-            
-            /*
-            isInputANumber() sjekker hvert symbol som blir skrevet inn i konsollen som første argument "args[0]" og
-            hvis symbolene ikke er et tall så returnerer metoden false.
+        }
 
-            Hvis det som skrives inn som første argument "args[0]" er tall, så returnerer metoden true.
-            */
-
-            static bool isInputANumber(string input) 
-            
-            {
-                foreach (var symbol in input)
-                {
-                    if (!char.IsDigit(symbol))
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }
-
-
-            static bool isInputADesiredLetter(string input)
-            {
-                foreach (var symbol in input)
-                {
-                    if(!(symbol == 'l' || symbol == 'L' || symbol == 'd' || symbol == 's'))
-                    {
-                        Console.WriteLine("Symbolene du brukte i args[1] tilfredsstiller ikke kravene");
-                        infoForUser();
-                        return false;
-                    }
-                }
-                return true;
-            }
-
-
-        static void infoForUser()
+        static void PasswordGenerator(string[] args)
         {
-            string info = "PasswordGenerator  \r\n  Options:\r\n  - l = lower case letter\r\n  - L = upper case letter\r\n  - d = digit\r\n  - s = special character (!\"#¤%&/(){}[]\r\nExample: PasswordGenerator 14 lLssdd\r\n         Min. 1 lower case\r\n         Min. 1 upper case\r\n         Min. 2 special characters\r\n         Min. 2 digits";
-            Console.WriteLine(info);
+            if (!isValid(args))
+            {
+                showHelpText();
+                return;
+            }
+            var length = Convert.ToInt32(args[0]);
+            var options = args[1];
+
+            var pattern = options.PadRight(length, 'l'); //fyller opp args[1] med 'l' til den har en lengde på 14 (args[0])
+            var password = string.Empty;
+
+            foreach (var category in pattern)
+            {
+                if (category == 'l') password += getRandomLowerCaseLetter();
+                else if (category == 'L') password += getRandomUpperCaseLetter();
+                else if (category == 'd') password += getRandomDigit();
+                else password += getRandomSpecialCharacter();
+            }
+            Console.WriteLine(password);
         }
+
+        static char getRandomSpecialCharacter()
+        {
+            var allSpecialCharacters = "!\"#¤%&/(){}[])";
+            var random = new Random();
+            var randomIndex = random.Next(0, allSpecialCharacters.Length-1);
+            return allSpecialCharacters[randomIndex];
         }
+
+        static char getRandomDigit()
+        {
+            var random = new Random();
+            return random.Next(0, 9).ToString()[0];
+        }
+
+        
+        private static char getRandomLetter(char min, char max)
+        {
+            var random = new Random();
+            return (char)random.Next(min, max); // Bruker casting (ta denne tingen og tolk det som en annen datatype).
+        }
+
+        static char getRandomLowerCaseLetter()
+        {
+            return getRandomLetter('a', 'z');
+        }
+        
+        static char getRandomUpperCaseLetter()
+        {
+            return getRandomLetter('A', 'Z');
+        }
+
+
+        static bool isValid(string[] args)
+            {
+                // Sjekk at antall argumenter er nøyaktig 2
+                if (args.Length != 2) return false;
+
+                var lengthString = args[0];
+                var options = args[1];
+
+
+                foreach (char charachter in options)
+                {
+                    //var acceptedCharacters = options.ToCharArray();
+                    //if (!acceptedCharacters.Contains(charachter)) return false;
+                    if(charachter != 'l'
+                      && charachter != 'L'
+                      && charachter != 'd'
+                      && charachter != 's')
+                    {
+                        return false;
+                    } 
+                }
+
+                foreach (var characther in lengthString)
+                {
+                    if (!char.IsDigit(characther))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+
+            static void showHelpText()
+            {
+                Console.WriteLine("PasswordGenerator");
+                Console.WriteLine("Options:");
+                Console.WriteLine("  - l = lowercase letter");
+                Console.WriteLine("  - L = uppercase letter");
+                Console.WriteLine("  - d = digit");
+                Console.WriteLine("  - s = special character (!\"#¤%&/(){}[])");
+                Console.WriteLine("Example: PasswordGenerator 14 lLssdd");
+                Console.WriteLine("         Min. 1 lowercase");
+                Console.WriteLine("         Min. 1 uppercase");
+                Console.WriteLine("         Min. 2 special characters");
+                Console.WriteLine("         Min. 2 digits");
+            }
     }
 }
+           
+    
+
+         
